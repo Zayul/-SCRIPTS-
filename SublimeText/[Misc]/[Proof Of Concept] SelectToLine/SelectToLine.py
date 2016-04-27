c3dfa9be7d507c6d248ec62a9ee6ab0a18bbfaf9
Enteleform <Enteleform@gmail.com>
2016-04-27 15:28:36 -0400

    Formatting

--- SublimeText/[Misc]/[Proof Of Concept] SelectToLine/SelectToLine.py
+++ SublimeText/[Misc]/[Proof Of Concept] SelectToLine/SelectToLine.py
@@ -1,32 +1,32 @@
-	class SelectToLineCommand( sublime_plugin.TextCommand ):
+class SelectToLineCommand( sublime_plugin.TextCommand ):
 
-		def run( self, edit ):
+	def run( self, edit ):
 
-			window     = self.view.window()
-			selections = self.view.sel()
+		window     = self.view.window()
+		selections = self.view.sel()
 
-			if len( selections ) != 1:
-				return
+		if len( selections ) != 1:
+			return
 
-			selection = selections[0]
-			self.selectionStart = selection.a
+		selection = selections[0]
+		self.selectionStart = selection.a
 
-			window.show_input_panel( "Select To Line Number", "", self.get_LineNumber, None, None )
+		window.show_input_panel( "Select To Line Number", "", self.get_LineNumber, None, None )
 
-		def get_LineNumber( self, userInput ):
+	def get_LineNumber( self, userInput ):
 
-			lineToRow_Offset = 1
-			row = int ( userInput ) - lineToRow_Offset
+		lineToRow_Offset = 1
+		row = int ( userInput ) - lineToRow_Offset
 
-			selectionStart = self.selectionStart
-			selectionEnd_Row   = self.view.text_point( row, 0 )
+		selectionStart = self.selectionStart
+		selectionEnd_Row   = self.view.text_point( row, 0 )
 
-			if selectionEnd_Row >= selectionStart:
-				selectionEnd = self.view.line( selectionEnd_Row ).b
-			elif selectionEnd_Row < selectionStart:
-				selectionEnd = self.view.line( selectionEnd_Row ).a
+		if selectionEnd_Row >= selectionStart:
+			selectionEnd = self.view.line( selectionEnd_Row ).b
+		elif selectionEnd_Row < selectionStart:
+			selectionEnd = self.view.line( selectionEnd_Row ).a
 
-			newSelection = sublime.Region ( self.selectionStart, selectionEnd )
+		newSelection = sublime.Region ( self.selectionStart, selectionEnd )
 
-			self.view.selection.clear()
-			self.view.selection.add( newSelection )
+		self.view.selection.clear()
+		self.view.selection.add( newSelection )
