5c9ada366d1b62d9ec6964585dd26836f7d79056
Enteleform <Enteleform@gmail.com>
2016-05-16 15:37:50 -0400

    added POC MoveTextToInactiveDocument

---
+++ SublimeText/[Misc]/[Proof Of Concept] MoveTextToInactiveDocument/MoveTextToInactiveDocument/MoveTextToInactiveDocument.py
@@ -0,0 +1,59 @@
+import sublime, sublime_plugin
+
+LINE_BREAK = "\n"
+
+class MoveTextToInactiveDocumentCommand( sublime_plugin.TextCommand ):
+	def run( self, edit, MODE = "copy" ):
+
+		view   = self.view
+		window = view.window()
+
+		#■■■  Verify Window Groups  ■■■#
+
+		windowGroup_Count = window.num_groups()
+
+		if windowGroup_Count != 2:
+			return
+
+		#■■■  Set Active & Inactive Documents  ■■■#
+
+		activeDocument_Group = window.active_group()
+
+		if activeDocument_Group == 0:
+			activeDocument   = window.active_view_in_group( 0 )
+			inactiveDocument = window.active_view_in_group( 1 )
+		elif activeDocument_Group == 1:
+			activeDocument   = window.active_view_in_group( 1 )
+			inactiveDocument = window.active_view_in_group( 0 )
+
+		#■■■  Verify Selection  ■■■#
+
+		selections = activeDocument.sel()
+
+		if len( selections ) == 0:
+			return
+
+		#■■■  Move Text From Active Document To Inactive Document  ■■■#
+
+		first_InsertionPoint = inactiveDocument.size()
+
+		for region in selections:
+
+			regionText     = activeDocument.substr( region ) + LINE_BREAK
+			insertionPoint = inactiveDocument.size()
+
+			if MODE.lower() == "cut":
+				activeDocument.erase( edit, region )
+
+			inactiveDocument.insert( edit, insertionPoint, regionText )
+
+		#■■■  Select & Scroll To Inserted Text  ■■■#
+
+		inactiveDocument_End = inactiveDocument.size()
+		insertedText_Region  = sublime.Region( first_InsertionPoint, inactiveDocument_End )
+
+		inactiveDocument.selection.clear()
+		inactiveDocument.selection.add( insertedText_Region )
+
+		inactiveDocument.show( inactiveDocument.size() )
+		window.focus_view( activeDocument )
---
+++ SublimeText/[Misc]/[Proof Of Concept] MoveTextToInactiveDocument/README.md
@@ -0,0 +1,39 @@
+Written in response to this [__StackOverflow question__](http://stackoverflow.com/questions/37248764/sublime-text-how-do-i-create-a-keyboard-shortcut-to-move-text-from-one-open-fi) asking how to cut text from one file to another ( *in a 2 group view layout* ).
+
+#Demo:
+
+![Demo](https://raw.githubusercontent.com/Enteleform/-SCRIPTS-/master/SublimeText/%5BMisc%5D/%5BProof%20Of%20Concept%5D%20MoveTextToInactiveDocument/Demo.gif)
+
+&nbsp;
+
+-----
+
+# Features:
+
+* bidirectional copy & cut commands ( *from active document to inactive document, works on either view* )
+* selects & scrolls to inserted text @ inactive document
+* only executes if there are exactly 2 view groups
+
+&nbsp;
+
+-----
+
+#Implementation:
+
+* verify window groups
+* set active & inactive documents
+* verify selection
+* move text from active document to inactive document
+* select & scroll to inserted text
+
+&nbsp;
+
+-----
+
+#Usage:
+
+* copy the plugin folder to your `Packages` directory
+* restart SublimeText
+* execute the commands via the included `key-bindings`:
+ * <kbd>Ctrl + Super + C</kbd> to copy selections to the inactive document
+ * <kbd>Ctrl + Super + X</kbd> to cut selections to the inactive document
