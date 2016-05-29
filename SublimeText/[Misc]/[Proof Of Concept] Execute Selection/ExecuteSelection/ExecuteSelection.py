63975289db6819aa63944a3f461f0c62bd2a2a82
Enteleform <enteleform@gmail.com>
2016-05-29 03:12:48 -0400

    improved formatting

--- SublimeText/[Misc]/[Proof Of Concept] Execute Selection/ExecuteSelection/ExecuteSelection.py
+++ SublimeText/[Misc]/[Proof Of Concept] Execute Selection/ExecuteSelection/ExecuteSelection.py
@@ -3,23 +3,20 @@ import subprocess
 import os
 import re
 
-class ExecuteSelectionCommand ( sublime_plugin.TextCommand ):
+class ExecuteSelectionCommand( sublime_plugin.TextCommand ):
 	def run( self, edit ):
 
-		plugin_FilePath = os.path.dirname ( __file__ ) + os.sep
-		plugin_FilePath = plugin_FilePath.replace ( "/", os.sep )
-		exec_File       = plugin_FilePath + "exec.py"
+		plugin_FilePath = os.path.dirname( __file__ ).replace( "/", os.sep )
+		exec_File       = plugin_FilePath + os.sep + "exec.py"
 
-		view = self.view
+		view      = self.view
 		selection = view.sel()[0]
 
-		script = view.substr ( selection )
-		script = script.strip()
+		script = view.substr( selection ).strip()
 		script = re.sub( "[\n\r]+", ";", script )
 
 		command  = [ "cmd.exe", "/c" ]
 		command += [ "python", "-u", exec_File, script ]
 		command += [ "&", "echo.", "&", "pause" ]
 
-		subprocess.Popen ( command )
-
+		subprocess.Popen( command )
